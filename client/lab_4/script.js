613360750dd3a68a1009cbb40fd816034b808244
kevipun1234 <47250967+kevipun1234@users.noreply.github.com>
2022-02-20 23:15:58 -0500

    lab_4

---
+++ client/lab_4/script.js
@@ -0,0 +1,54 @@
+let slidePosition = 0;
+const slides = document.querySelectorAll('.carousel_item');
+const totalSlides = slides.length;
+
+
+
+const next = document.querySelector('#carousel_button--next')
+    next.addEventListener("click", () => {
+        moveNext();
+    });
+const prev = document.querySelector('#carousel_button--prev')
+    prev.addEventListener("click", () => {
+        movePrev();
+    });
+
+
+function updateSlidePosition() {
+    for (let slide of slides) {
+        slide.classList.remove('carousel_item--visible');
+        slide.classList.add('carousel_item--hidden');
+    }
+        slides[slidePosition].classList.add('carousel_item--visible');
+    
+}
+function moveNext() {
+
+    if (slidePosition === totalSlides - 1) {
+         slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+    updateSlidePosition()
+}
+
+function movePrev() {
+   
+
+    if (slidePosition === 0) {
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePosition--;
+        }
+        updateSlidePosition()
+    }
+
+function clickToChangebutton() {
+    document.addEventListener('click', (event) => {
+        const box = document.querySelector('#carousel_button--prev');
+        const classes = box.classList;
+        console.log(classes.value);
+        const newClass = (classes.value === "#photo-grid-container #carousel_actions button") ? "afterClick" : "button";
+        box.classList = newClass;
+    });
+}
